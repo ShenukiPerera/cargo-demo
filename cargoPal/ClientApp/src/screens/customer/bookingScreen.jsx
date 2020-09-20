d3e180ea6952855f8ae3fe6baeb159ebd4145608
ShenukiPerera <35036780+ShenukiPerera@users.noreply.github.com>
2020-09-20 22:05:15 +0530

    Agent and customer screens

---
+++ cargoPal/ClientApp/src/screens/customer/bookingScreen.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import Screen from '../../components/screen';
+import Booking from '../../components/booking';
+
+class BookingScreen extends Component {
+  state = {
+    user: {
+      firstName: 'will',
+      lastName: 'smith',
+      address: 'home',
+      phone: '123456789',
+    },
+    schedule: {
+      id: 1,
+      destination: 'china',
+    },
+  };
+
+  componentDidMount() {
+    // get the current user detials
+  }
+
+  render() {
+    return (
+      <Screen title="Booking" subtitle="Make your bookings here.">
+        <Booking user={this.state.user} schedule={this.state.schedule} />
+      </Screen>
+    );
+  }
+}
+
+export default BookingScreen;
