6deb86fa16232fafdf23ba1d765a6f1aa33fb24e
ShenukiPerera <35036780+ShenukiPerera@users.noreply.github.com>
2020-10-01 23:16:51 +0530

    updated screens

--- cargoPal/ClientApp/src/screens/customer/ShipmentsScreen.jsx
+++ cargoPal/ClientApp/src/screens/customer/ShipmentsScreen.jsx
@@ -1,32 +1,84 @@
 import React, { Component } from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import { connect } from "react-redux";
+
 import Screen from "../../components/screen";
+import Shipment from "../../components/shipment";
+
+import { GetShipment } from "../../actions/shipmentAction";
+import SearchShipment from "../../components/searchShipments";
 
 class ShipmentsScreen extends Component {
   state = {
     shipments: [],
+    search: [],
   };
 
   componentDidMount() {
     // get all shipments from all agents where the status is available
+    this.props.GetShipment();
   }
 
-  componentDidUpdate() {
+  componentDidUpdate(prevProps) {
     // update the state with the new shipments
+    if (prevProps.shipments.data !== this.props.shipments.data) {
+      this.setState({ shipments: this.props.shipments.data });
+    }
   }
+  //searching for shipments using to and from fields
+  handleSearch = ({ to, from }) => {
+    console.log({ to, from });
+    const shipments = this.state.shipments;
+
+    this.setState({
+      search: shipments.filter(
+        (shipment) => shipment.origin === from && shipment.destination === to
+      ),
+    });
+  };
+
+  handleReset = () => {
+    this.setState({ search: [] });
+  };
 
   render() {
-    const { shipments } = this.state;
+    const { shipments, search } = this.state;
 
     return (
       <Screen
         title="View Availbale Shipments"
         subtitle="The user can select the shipment and agent here."
       >
+        <SearchShipment
+          handleSearch={this.handleSearch}
+          handleReset={this.handleReset}
+        />
+        <hr />
         {/* {Shipment compoenents will rendered here with their prices and stuff.. } */}
-        {shipments.map((shipment) => console.log(shipment))}
+
+        <Container style={{ overflow: "hidden" }}>
+          <Row>
+            {search.length > 0 &&
+              search.map((search) => (
+                <Col sm={3} key={search.shipmentId}>
+                  <Shipment key={search} shipment={search} />
+                </Col>
+              ))}
+          </Row>
+          <Row>
+            {search.length <= 0 &&
+              shipments.map((shipment) => (
+                <Col sm={3} key={shipment.shipmentId}>
+                  <Shipment key={shipment} shipment={shipment} />
+                </Col>
+              ))}
+          </Row>
+        </Container>
       </Screen>
     );
   }
 }
 
-export default ShipmentsScreen;
+const mapStateToProps = ({ shipments }) => ({ shipments });
+
+export default connect(mapStateToProps, { GetShipment })(ShipmentsScreen);
--- cargoPal/ClientApp/src/screens/customer/bookingScreen.jsx
+++ cargoPal/ClientApp/src/screens/customer/bookingScreen.jsx
@@ -1,30 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Screen from '../../components/screen';
-import Booking from '../../components/booking';
+import Screen from "../../components/screen";
+import BookingForm from "../../components/bookingform";
 
 class BookingScreen extends Component {
-  state = {
-    user: {
-      firstName: 'will',
-      lastName: 'smith',
-      address: 'home',
-      phone: '123456789',
-    },
-    schedule: {
-      id: 1,
-      destination: 'china',
-    },
-  };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
   componentDidMount() {
     // get the current user detials
   }
 
+  // handleSellerBidUpdate = (childData) => {
+  //   const backupUpdate = this.state.sellerBids;
+  //   backupUpdate.forEach((sellerBid) => {
+  //     if (sellerBid.sellerBidId === childData.sellerBidId) {
+  //       sellerBid.status = childData.status;
+  //       sellerBid.bestPrice = childData.bestPrice;
+  //     }
+  //   });
+  //   this.props.UpdateSellerBid(childData);
+  //   if (this.props.sellerBids.hasError) {
+  //     this.setState({ sellerBids: backupUpdate });
+  //   }
+  // };
+
   render() {
     return (
       <Screen title="Booking" subtitle="Make your bookings here.">
-        <Booking user={this.state.user} schedule={this.state.schedule} />
+        <BookingForm schedule={this.state.schedule} />
       </Screen>
     );
   }
